b8261d37dee029f426184626b8e929177b8eacd9
chen <1111>
2021-06-02 15:19:02 +0800

    我的页面

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,15 +1,63 @@
 <template>
   <div>
-      我的
+    <van-cell-group>
+      <van-field
+        v-model="moddie"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="code"
+        clearable
+        left-icon="music-o"
+        placeholder="请输入密码"
+      >
+        <template #button>
+          <van-button
+            v-if="verificationCode"
+            size="small"
+            @click="sendVerificationCode"
+            type="primary"
+            >发送验证码</van-button
+          >
+          <van-count-down
+            @finish="finish"
+            v-else
+            :time="time"
+            millisecond
+            format="ss s"
+          />
+        </template>
+      </van-field>
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      moddie: "",
+      code: "",
+      verificationCode: true,
+      time: 10 * 1000,
+    };
+  },
+  methods: {
+    sendVerificationCode() {
+      this.verificationCode = false;
+    },
+    finish() {
+      this.verificationCode = true;
+     
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style>
-
+.van-cell {
+  align-items: center;
+}
 </style>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,15 +1,65 @@
 <template>
-  <div>
-      搜索
+  <div class="search">
+    <van-nav-bar title="标题" class="header"> </van-nav-bar>
+    <van-field
+      v-model="fieldValue"
+      is-link
+      readonly
+      label="地区"
+      placeholder="请选择所在地区"
+      @click="show = true"
+    />
+    <van-popup v-model="show" round position="bottom">
+      <van-cascader
+        v-model="cascaderValue"
+        title="请选择所在地区"
+        :options="options"
+        @close="show = false"
+        @finish="onFinish"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      show: false,
+      fieldValue: "",
+      cascaderValue: "",
+      // 选项列表，children 代表子选项，支持多级嵌套
+      options: [
+        {
+          text: "浙江省",
+          value: "330000",
+          children: [{ text: "杭州市", value: "330100" }],
+        },
+        {
+          text: "江苏省",
+          value: "320000",
+          children: [{ text: "南京市", value: "320100" }],
+        },
+      ],
+    };
+  },
+  methods: {
+    // 全部选项选择完毕后，会触发 finish 事件
+    onFinish({ selectedOptions }) {
+      this.show = false;
+      this.fieldValue = selectedOptions.map((option) => option.text).join("/");
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang='less' scope>
+.search {
+  .header {
+    background: #0094ff;
+    .van-nav-bar__title {
+      color: #fff;
+    }
+  }
+}
 </style>
